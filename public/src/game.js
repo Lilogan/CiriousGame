1032dc3e38052c1c46b0f771277cb7b313da6b45
Unknown <ohcesmoi@gmail.com>
2019-01-19 01:19:06 +0100

    Preview du bâtiment au survol et pose au click
    Event listener add sur les tiles du sol

--- public/src/game.js
+++ public/src/game.js
@@ -5,6 +5,9 @@ let pointer;
 let game;
 let config;
 let mapObjects;
+let tmpSprite;
+let newSprite;
+
 config = {
     type: Phaser.AUTO,
     width: windowWidth,
@@ -25,9 +28,6 @@ config = {
 
 game = new Phaser.Game(config);
 
-
-
-
 function preload() {
     this.load.crossOrigin = 'Anonymous';
     //load all necessary assets
@@ -42,19 +42,17 @@ function preload() {
 }
 
 function create() {
-
-    let mapSize = 50;
     let mapData = [
         //posX, posY, dataType
-        [24,27,"shop1"],
         [24,26,"shop1"],
+        [24,27,"shop1"],
         [23,27,"roadNS"],
         [23,26,"roadNS"],
         [23,28,"roadNE"],
         [22,28,"roadEW"],
-
     ];
 
+    let mapSize = 50;
     mapObjects = this.add.group();
     let borderOffset = new Phaser.Geom.Point(windowWidth/2, 50/2+windowHeight/2-50*Math.floor(mapSize/2)); // define the offset to center the map
 
@@ -65,11 +63,25 @@ function create() {
             point.x = j * spriteWidth;
             point.y = i * spriteWidth;
             let isoPoint = fromCartToIso(point);
-            this.add.sprite(isoPoint.x + borderOffset.x, isoPoint.y + borderOffset.y, "floor", false).setOrigin(0.5,1);
+            let sprite = this.add.sprite(isoPoint.x + borderOffset.x, isoPoint.y + borderOffset.y + 14, "floor", false).setOrigin(0.5,1);
 
+            // Preview d'un shop quand le curseur passe au dessus d'une tile, add le shop au click
+            sprite.setInteractive({pixelPerfect: true});
+            sprite.on("pointerover", () => {
+                tmpSprite = this.add.sprite(isoPoint.x + borderOffset.x, isoPoint.y + borderOffset.y, "shop1", false).setOrigin(0.5,1);
+                tmpSprite.alpha = 0.7;
+                tmpSprite.depth = tmpSprite.y;
+            }, this);
+            sprite.on("pointerout", () => {
+                tmpSprite.destroy();
+            }, this);
+            sprite.on("pointerdown", () => {
+                newSprite = this.add.sprite(isoPoint.x + borderOffset.x, isoPoint.y + borderOffset.y, "shop1", false).setOrigin(0.5,1);
+                newSprite.depth = newSprite.y;
+            }, this);
         }
     }
-
+    
     //display elements
     mapData.forEach((curData) => {
         let spriteType = curData[2];
@@ -78,9 +90,8 @@ function create() {
         point.x = curData[1] * spriteWidth;
         point.y = curData[0] * spriteWidth;
         let isoPoint = fromCartToIso(point);
-        let sprite = this.add.sprite(isoPoint.x + borderOffset.x, isoPoint.y + borderOffset.y-14, spriteType, false).setOrigin(0.5,1);
+        let sprite = this.add.sprite(isoPoint.x + borderOffset.x, isoPoint.y + borderOffset.y, spriteType, false).setOrigin(0.5,1);
         sprite.depth = sprite.y;
-
     });
 
     // Zoom camera
