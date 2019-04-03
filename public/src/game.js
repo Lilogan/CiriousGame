f87b860f9b6059eaf9be98f02bc52f9648dc243d
Unknown <ohcesmoi@gmail.com>
2019-04-03 16:13:47 +0200

    Bugs fixed

---
+++ public/src/game.js
@@ -0,0 +1,19 @@
+let GameScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize: function GameScene() {
+        Phaser.Scene.call(this, {key: "GameScene", active: true});
+    },
+    preload: function () {
+
+    },
+    create: function () {
+
+    },
+    update: function () {
+
+    }
+});
+
+export default GameScene;
---
+++ public/src/hud.js
@@ -0,0 +1,19 @@
+let HudScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize: function HudScene() {
+            Phaser.Scene.call(this, {key: "HudScene", active: true});
+    },
+    preload: function () {
+
+    },
+    create: function () {
+
+    },
+    update: function () {
+
+    }
+});
+
+export default HudScene;
